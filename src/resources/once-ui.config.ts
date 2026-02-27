db1321836e4527d2f3a06891dd94110f686508d7
Zahid Abdillah <zahidabdillah@protonmail.com>
2026-02-27 13:27:15 +0700

    chore: set production baseURL and finalize deployment

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -14,7 +14,7 @@ import {
 import { home } from "./index";
 
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL: string = "https://demo.magic-portfolio.com";
+const baseURL: string = "https://zahidabdillah.dev";
 
 const routes: RoutesConfig = {
   "/": true,
